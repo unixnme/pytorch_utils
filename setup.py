3e93b9dcd0c47b152d80e0c7e174db8bd21a561c
Unix&Me <unixnme@gmail.com>
2019-03-02 19:01:15 -0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pytorch_utils',
+    version='0.1.0',
+    author='Young Mo Kang',
+    author_email='kang.youngmo@gmail.com',
+    packages=['logger'],
+    scripts=[],
+    description='Useful utilities for training with Pytorch.',
+    install_requires=[
+        "torch >= 0.4",
+    ],
+)
